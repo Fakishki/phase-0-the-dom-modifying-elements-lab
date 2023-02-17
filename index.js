c679f5d3da8446b0aab7f704c85f0fba84144e34
Wally Pfingsten <wallypfingsten@gmail.com>
2023-02-17 09:54:50 -0800

    Wally is the champion

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//Write your code here
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const userName = 'Wally';
+newHeader.textContent = `${userName} is the champion`;
+
+document.body.appendChild(newHeader);
